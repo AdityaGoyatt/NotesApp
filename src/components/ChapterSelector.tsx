de96837646d2b8e41e34753b3355ec7440fcce13
Aditya <aditya.goyat023@gmail.com>
2023-07-01 19:30:44 -0700

    Error Page Setted Up

--- src/components/ChapterSelector.tsx
+++ src/components/ChapterSelector.tsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
+import useChapters from "../hooks/useChapters";
 
 const ChapterSelector = () => {
   const { slug: providedSlug } = useParams();
+  const data = useChapters();
+  const chapters = data.find((chptr) => chptr.slug === providedSlug);
 
   return <div>ChapterSelector</div>;
 };
