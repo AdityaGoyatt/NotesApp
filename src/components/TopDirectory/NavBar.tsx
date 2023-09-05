d45106cab759f4524ecf982630397107cf442d71
Aditya <aditya.goyat023@gmail.com>
2023-09-04 17:15:26 -0700

    UI Optimised, Logo Image Changed to dark mode bttn

--- src/components/TopDirectory/NavBar.tsx
+++ src/components/TopDirectory/NavBar.tsx
@@ -1,15 +1,19 @@
-import { Box, HStack, Heading, Image } from "@chakra-ui/react";
+import { Box, HStack, Heading, Image, useColorMode } from "@chakra-ui/react";
 import logo from "../../assets/logo.png";
 import AddButton from "../Buttons/AddButton";
-import AddCourse from "../../Archieves/AddCourse";
-import { useState } from "react";
 
 const NavBar = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <>
       <Box background=" linear-gradient(to right, #c33764, #1d2671)">
         <HStack justifyContent="space-between" marginX={3} paddingY={2}>
-          <Image borderRadius="40%" boxSize="60px" src={logo}></Image>
+          <Image
+            onClick={toggleColorMode}
+            borderRadius="40%"
+            boxSize="60px"
+            src={logo}
+          ></Image>
           <Heading fontSize="2xl" fontFamily="Caprasimo">
             My Notes
           </Heading>
